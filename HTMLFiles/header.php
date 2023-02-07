acc2462b59dd3ccde018a992aadec6467c80e6ee
Hakim Ullah <106232159+Hakimullah92@users.noreply.github.com>
2023-02-07 22:03:04 +0500

    Making Header at Desktop view

---
+++ HTMLFiles/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-shopper</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../CSS_Files/header.css">
+    <script src="../JS_Files/header.js" defer></script>
+</head>
+<body>
+    <!-- Header Start -->
+    <header class="header">
+        <nav class="navbar">
+            <div class="navbar_left_content">
+                <a href="#"><h3 class="logo"><i class="fas fa-dumpster-fire"></i> <span>E-Shopper</span></h3></a>
+                <ul class="navbar-list">
+                    <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Shop</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Cart</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">FaQ's</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                </ul>
+            </div>
+            <div class="navbar_right_content">
+            <i class="fas fa-user" id="account"></i>
+            <i class="fas fa-cart-plus" id="cart"></i>
+            <i class="fas fa-th-large" id="menu"></i>
+            </div>
+        </nav>
+    </header>
+    <!-- Header End -->
+</body>
+</html>
